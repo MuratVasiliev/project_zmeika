721787e7c8a7d35b36c991e357409fe221158cc2
boris <borispak2005@gmail.com>
2022-12-19 00:26:47 +0300

    added class menu

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import Varriabl
 
 class Menu:
     def __init__(self, game):
+        """Создаем курсор, который будет двигаться по меню"""
         self.game = game
         self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
         self.run_display = True
