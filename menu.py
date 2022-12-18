cef7ce4ad6ec09e7c3f7f0159e39071fc4c33e61
boris <borispak2005@gmail.com>
2022-12-19 00:22:58 +0300

    added class menu

--- menu.py
+++ menu.py
@@ -1,4 +1,10 @@
 import pygame
 import Varriabl
 
-de
+class Menu:
+    def __init__(self, game):
+        self.game = game
+        self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
+        self.run_display = True
+        self.cursor_rect = pygame.Rect(0, 0, 20, 20)
+        self.offset = - 100
