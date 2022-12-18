5da12fcb399155bd49d7dfb92427ea13d49374fc
boris <borispak2005@gmail.com>
2022-12-19 00:41:29 +0300

    added def draw_cursor

--- menu.py
+++ menu.py
@@ -12,3 +12,22 @@ class Menu:
     def draw_cursor(self):
         """Рисуем курсор в форме звезды на экране"""
         self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
+
+    def blit_screen(self):
+        """Отрисовываем постоянную картинку"""
+        self.game.window.blit(self.game.display, (0, 0))
+        pygame.display.update()
+        self.game.reset_keys()
+
+
+class MainMenu(Menu):
+    def __init__(self, game):
+        """Пишем положение курсора на разных позициях"""
+        Menu.__init__(self, game)
+        self.state = "Start"
+        self.startx, self.starty = self.mid_w, self.mid_h + 30
+        self.levelx, self.levely = self.mid_w, self.mid_h + 50
+        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 70
+        self.creditsx, self.creditsy = self.mid_w, self.mid_h + 90
+        self.scorex, self.scorey = self.mid_w, self.mid_h + 110
+        self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
