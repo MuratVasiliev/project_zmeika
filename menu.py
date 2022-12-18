2a71d178679efdc9d031d71c9158ab52e7201dbc
boris <borispak2005@gmail.com>
2022-12-19 02:26:03 +0300

    added level menu

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import pygame
 import Varriabl
 
+
 class Menu:
     def __init__(self, game):
         """Создаем курсор, который будет двигаться по меню"""
@@ -9,6 +10,7 @@ class Menu:
         self.run_display = True
         self.cursor_rect = pygame.Rect(0, 0, 20, 20)
         self.offset = - 100
+
     def draw_cursor(self):
         """Рисуем курсор в форме звезды на экране"""
         self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
@@ -48,6 +50,7 @@ class MainMenu(Menu):
                 self.game.draw_text("Score", 20, self.scorex, self.scorey)
                 self.draw_cursor()
                 self.blit_screen()
+
     def move_cursor(self):
         """Прописываем движение курсора при нажатии клавиш"""
         if self.game.DOWN_KEY:
@@ -93,4 +96,45 @@ class MainMenu(Menu):
                     self.levelx + self.offset, self.levely)
                 self.state = 'Level'
 
+    def check_input(self):
+        """Переход при нажатии на пункт меню"""
+        self.move_cursor()
+        if self.game.START_KEY:
+            if self.state == 'Start':
+                self.game.playing = True
+            elif self.state == 'Level':
+                self.game.curr_menu = self.game.level
+            elif self.state == 'Options':
+                self.game.curr_menu = self.game.options
+            elif self.state == 'Credits':
+                self.game.curr_menu = self.game.credits
+            elif self.state == 'Score':
+                self.game.curr_menu = self.game.score
+            self.run_display = False
+
+
+class LevelMenu(Menu):
+    """Прописываем положение курсора в Level Menu"""
+    def __init__(self, game):
+        Menu.__init__(self, game)
+        self.state = 'Level 1'
+        self.l1x, self.l1y = self.mid_w, self.mid_h + 20
+        self.l2x, self.l2y = self.mid_w, self.mid_h + 40
+        self.l3x, self.l3y = self.mid_w, self.mid_h + 60
+        self.cursor_rect.midtop = (self.l1x + self.offset, self.l1y)
+
+    def display_menu(self):
+        """Прописываем меню при нажатии на Level в главном меню"""
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text('Level', 20, self.game.DISPLAY_W / 2,
+                                self.game.DISPLAY_H / 2 - 30)
+            self.game.draw_text("Level 1", 15, self.l1x, self.l1y)
+            self.game.draw_text("Level 2", 15, self.l2x, self.l2y)
+            self.game.draw_text("Level 3", 15, self.l3x, self.l3y)
+            self.draw_cursor()
+            self.blit_screen()
 
