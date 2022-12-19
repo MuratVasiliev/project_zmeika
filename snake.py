072579b0bbd85b93e907df9405ce236bb8c36f1f
FlexxofIvan <stecsov.vanys@mail.ru>
2022-12-19 04:47:24 +0300

    do engine

--- snake.py
+++ snake.py
@@ -1,7 +1,8 @@
 import pygame
-import random 
+import random
 import pygame
 
+dir = [1, 0]
 w = 800
 h = 600
 rows = 20
@@ -25,24 +26,41 @@ fps = 5
 #             exit()
 ###
 class Cube:
-    def __init__(self):
-        dis = w // rows
-        self.head_surf = pygame.Surface((dis, dis))
-        self.head_surf.fill((0, 0, 255))
-        self.head_rect = self.head_surf.get_rect(topleft=(w / 2, (h / 2)-dis/2))
-        self.pos = [self.head_rect.left, self.head_rect.top]
-        self.vx = dis
-        self.vy = 0
+    def __init__(self, pos):
+        self.pos = pos
+        self.surf = pygame.Surface((dis, dis))
+        self.surf.fill((0, 0, 255))
+        self.rect = self.surf.get_rect(topleft=pos)
+        self.pos = [self.rect.left, self.rect.top]
+
+        # self.vx = dis
+        # self.vy = 0
 
     # def blit_screen(self):
     #     self.game.window.blit(self.game.display, (0, 0))
+cube1 = Cube([0,0])
+
+class Snake:
+    def __init__(self):
+        self.body = [cube1]
+        self.head = self.body[len(self.body)-1]
+        self.tail = self.body[0]
+
+    def add_Cube(self, pos):
+        self.body.append(0)
+        self.head = self.body[len(self.body)-1]
+        self.head = Cube(pos)
 
     def move(self):
-        self.pos = [self.pos[0] + self.vx, self.pos[1] + self.vy]
-        self.head_rect = self.head_surf.get_rect(topleft=self.pos)
-        if self.head_rect.left >= 800:
-            self.pos = [-dis, self.pos[1] + self.vy]
-        screen.blit(self.head_surf, self.head_rect)
+        self.tail.pos = [self.head.pos[0] + dir[0], self.head.pos[1] + dir[1]]
+
+    def draw(self):
+        for i in self.body:
+            i.surf = pygame.Surface((dis, dis))
+            i.surf.fill((0, 0, 255))
+            i.rect = i.surf.get_rect(topleft=i.pos)
+            screen.blit(i.surf, i.rect)
+
 
 def draw_grid(w, rows, surface):
     size_btwn = w // rows
@@ -56,14 +74,31 @@ def draw_grid(w, rows, surface):
         pygame.draw.line(surface, (100, 100, 100), (x, 0), (x, w))
         pygame.draw.line(surface, (100, 100, 100), (0, y), (w, y))
 
-c = Cube()
+
+def re_dir(events):
+    for i in events:
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP and dir != [0, 1]:
+                dir = [0, -1]
+            if i.key == pygame.K_DOWN and dir != [0, -1]:
+                dir = [0, 1]
+            if i.key == pygame.K_LEFT and dir != [1, 0]:
+                dir = [-1, 0]
+            if i.key == pygame.K_RIGHT and dir != [-1, 0]:
+                dir = [1, 0]
+
+s = Snake()
+
 
 while sn_running:
     pygame.display.update()
     clock.tick(fps)
     screen.fill((0, 0, 0))
     draw_grid(w, rows, screen)
-    c.move()
+
+    s.move()
+    s.draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
