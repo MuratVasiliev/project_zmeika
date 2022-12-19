ed6cddc4b4e137e1c05d6b845e3ead57ea97dc2c
FlexxofIvan <stecsov.vanys@mail.ru>
2022-12-19 19:42:06 +0300

    nothing really important

--- snake.py
+++ snake.py
@@ -20,7 +20,7 @@ clock = pygame.time.Clock()
 fps = 15
 foodx = w // 2
 foody = w // 2
-
+game_over_screen = pygame.image.load('game_over.jpg')
 
 class Cube:
     def __init__(self, pos):
@@ -51,12 +51,12 @@ class Snake:
 
     def draw(self):
         for i in self.body:
-            if i != self.body[len(self.body) - 1]: #кроме головы
+            if i != self.body[len(self.body) - 1]:  # кроме головы
                 i.surf = pygame.Surface((dis, dis))
                 i.surf.fill(pygame.Color('#D03E3A'))
                 i.rect = i.surf.get_rect(topleft=i.pos)
                 screen.blit(i.surf, i.pos)
-            else: #голова
+            else:  # голова
                 i.surf = pygame.Surface((dis, dis))
                 i.surf.fill(pygame.Color('#FF0000'))
                 i.rect = i.surf.get_rect(topleft=i.pos)
@@ -68,36 +68,35 @@ class Food:
         self.x = w // 2
         self.y = w // 2
 
-    def rand(self): #рандомайзер еды
+    def rand(self):  # рандомайзер еды
         self.x = random.randrange(1, rows) * dis
         self.y = random.randrange(1, rows) * dis
 
-    def draw(self, screen, dis): #отрисовка еды
+    def draw(self, screen, dis):  # отрисовка еды
         pygame.draw.rect(screen, pygame.Color('#3AD044'), [self.x, self.y, dis, dis])
 
-    def eat(self, snakex, snakey): #если съел
+    def eat(self, snakex, snakey):  # если съел
         if snakex == self.x and snakey == self.y:
             while self.check():
                 self.rand()
             screen.fill(pygame.Color('#A5FFAB'))
             s.add_Cube()
             print('eated')
+
     def check(self):
-        for i in range(len(s.body)):
-            if self.x == s.body[i].pos[0] and self.y == s.body[i-1].pos[1]:
+        for cube_number in range(len(s.body)):
+            if self.x == s.body[cube_number].pos[0] and self.y == s.body[cube_number - 1].pos[1]:
                 return True
             else:
                 return False
 
 
-
-
-def draw_grid(w, rows, surface): #сетка
+def draw_grid(w, rows, surface):  # сетка
     size_btwn = w // rows
 
     x = 0
     y = 0
-    for l in range(rows):
+    for line_number in range(rows):
         x = x + size_btwn
         y = y + size_btwn
 
@@ -105,35 +104,37 @@ def draw_grid(w, rows, surface): #сетка
         pygame.draw.line(surface, GREY, (0, y), (w, y))
 
 
-s = Snake() #инициализация анаконды
-f = Food(w) #яблОчко
+s = Snake()  # инициализация анаконды
+f = Food(w)  # яблОчко
 while sn_running:
     pygame.display.update()
     clock.tick(fps)
     screen.fill(pygame.Color('#EFFFA5'))
 
-    for i in pygame.event.get(): # изменение направления и приращение попы на пробел
-        if i.type == pygame.KEYDOWN:
-            if i.key == pygame.K_UP and dir != [0, 1]:
+    for event in pygame.event.get():  # изменение направления и приращение попы на пробел
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and dir != [0, 1]:
                 dir = [0, -1]
-            if i.key == pygame.K_DOWN and dir != [0, -1]:
+            if event.key == pygame.K_DOWN and dir != [0, -1]:
                 dir = [0, 1]
-            if i.key == pygame.K_LEFT and dir != [1, 0]:
+            if event.key == pygame.K_LEFT and dir != [1, 0]:
                 dir = [-1, 0]
-            if i.key == pygame.K_RIGHT and dir != [-1, 0]:
+            if event.key == pygame.K_RIGHT and dir != [-1, 0]:
                 dir = [1, 0]
-            if i.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE:
                 s.add_Cube()
-        if i.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             exit()
+
     f.check()
-    f.draw(screen, dis) #отрисовка еды
-    f.eat(s.body[len(s.body) - 1].pos[0], s.body[len(s.body) - 1].pos[1]) #проверка на съедение
-    s.move() #хуюв
-    for i in s.body[:-1]: #проверка на самопересечение
-        if s.head.pos == i.pos:
-            screen.fill(pygame.Color('#FF8A87'))
+    f.draw(screen, dis)  # отрисовка еды
+    f.eat(s.body[len(s.body) - 1].pos[0], s.body[len(s.body) - 1].pos[1])  # проверка на съедение
+    s.move()  # хуюв
+    for event in s.body[:-1]:  # проверка на самопересечение
+        if s.head.pos == event.pos:
+
+            screen.blit(game_over_screen, (400,400))
             print('fuck')
 
-    s.draw() #хуёу
+    s.draw()  # хуёу
     draw_grid(w, rows, screen)
