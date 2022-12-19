ce9b09e0b0519ce50ecd0c07a0ff920453ca4e07
FlexxofIvan <stecsov.vanys@mail.ru>
2022-12-19 23:08:37 +0300

    added class Button

--- classes.py
+++ classes.py
@@ -30,3 +30,24 @@ class BullVariables:
 
     def getter(self):
         return self._bul
+
+class Button:
+
+    def init(self, x, y, xsize, ysize, text):
+        """ constructor of class "Button" """
+        self.x = x
+        self.y = y
+        self.text = text
+        self.xsize = xsize
+        self.ysize = ysize
+
+    def is_click(self, event):
+        """ return True if you click on the Button """
+        if self.x < event.pos[0] < self.x + self.xsize and self.y < event.pos[1] < self.y + self.ysize:
+            return True
+
+    def write_text_on_button(self, screen):
+        """ writing text on the element of class "Button" """
+        my_font = pg.font.SysFont("monospace", 30)
+        text = my_font.render(str(self.text), 1, (255, 255, 255))
+        screen.blit(text, (self.x, self.y))
