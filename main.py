cb792dbffc3d0bf170d43cf8530ce32d5b4d83cf
MuratVasiliev <vasiliymuratov04@gmail.com>
2022-12-20 01:22:42 +0300

    added_backspace

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import menu
 import snake as sn
 import classes as cl
-
+import slitherio as sl
 running = cl.BullVariables(True)
 
 while running.getter():
@@ -11,3 +11,6 @@ while running.getter():
         menu.levelmenu(running)
     if sn.sn_running.getter():
         sn.snake_loop()
+    if sl.sn1_running.getter():
+        sl.snake_loop1()
+    #if sl.sn1_running.getter():
