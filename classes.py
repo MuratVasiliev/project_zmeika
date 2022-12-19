ddd7f0d26a2d2c911cd91d9fce2f7b0b05c9ee27
MuratVasiliev <vasiliymuratov04@gmail.com>
2022-12-19 23:07:47 +0300

    added_classes

--- classes.py
+++ classes.py
@@ -12,3 +12,21 @@ class NumVariables:
 
     def getter(self):
         return self._num
+
+class BullVariables:
+    """Class of Bullian variables"""
+
+    def __init__(self, init_bul=True):
+        self._bul = init_bul
+
+    def setter(self, arg):
+        self._bul = arg
+
+    def changer(self):
+        if self._bul:
+            self._bul = False
+        else:
+            self._bul = True
+
+    def getter(self):
+        return self._bul
