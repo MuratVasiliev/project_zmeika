b955af04436453d067cf567f684b8f7735e316ce
MuratVasiliev <vasiliymuratov04@gmail.com>
2022-12-19 17:09:49 +0300

    added_dvizok

--- snake.py
+++ snake.py
@@ -2,91 +2,88 @@ import pygame
 import random
 import pygame
 
-BLACK=(0,0,0)
-GREEN=(0, 255, 50)
-RED=(200,30, 30)
-BLUE=(0, 0, 255)
-GREY=(100, 100, 100)
-WHITE= (255, 255, 255)
-
-q=1
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 50)
+RED = (0, 0, 255)
+BLUE = (255, 0, 0)
+GREY = (100, 100, 100)
 dir = [1, 0]
 w = 800
-h = 600
+# h = 600
 rows = 20
 dis = w // rows
 pygame.init()
 sn_running = True
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((w, w))
 all_sprites = pygame.sprite.Group()
 clock = pygame.time.Clock()
-fps = 5
+fps = 15
+foodx = w // 2
+foody = w // 2
 
 
-# head_surf = pygame.Surface((50, 50))
-# head_rect = head_surf.get_rect(midbottom=(400, 300))
-# head_surf.fill((0, 0, 255))
-# screen.blit(head_surf, head_rect)
-# while sn_running:
-#     pygame.display.update()
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             exit()
-###
 class Cube:
     def __init__(self, pos):
         self.pos = pos
-        self.surf = pygame.Surface((dis, dis))
-        self.surf.fill(BLUE)
-        self.rect = self.surf.get_rect(topleft=pos)
-        self.pos = [self.rect.left, self.rect.top]
 
-        # self.vx = dis
-        # self.vy = 0
-
-    # def blit_screen(self):
-    #     self.game.window.blit(self.game.display, (0, 0))
-cube1 = Cube([0,0])
 
 class Snake:
     def __init__(self):
-        self.body = [cube1]
-        self.head = self.body[len(self.body)-1]
-        self.tail = self.body[0]
+        self.body = []
+        self.body = [Cube([0, 0])]
 
-    def add_Cube(self, pos):
-        self.body.append(0)
-        self.head = self.body[len(self.body)-1]
-        self.head = Cube(pos)
+    def add_Cube(self):
+        self.body.insert(0, Cube(self.body[0].pos))
 
     def move(self):
-        self.tail.pos = [self.head.pos[0] + dir[0], self.head.pos[1] + dir[1]]
+        self.head = self.body[len(self.body) - 1]
+        self.body.append(Cube([self.head.pos[0] + dir[0] * dis, self.head.pos[1] + dir[1] * dis]))
+        self.head = self.body[len(self.body) - 1]
+        self.body.pop(0)
+        if self.head.pos[0] >= w:
+            self.head.pos[0] = 0
+        if self.head.pos[0] <= -dis:
+            self.head.pos[0] = w - dis
+        if self.head.pos[1] >= w:
+            self.head.pos[1] = 0
+        if self.head.pos[1] <= -dis:
+            self.head.pos[1] = w - dis
 
     def draw(self):
         for i in self.body:
-            i.surf = pygame.Surface((dis, dis))
-            i.surf.fill((0, 0, 255))
-            i.rect = i.surf.get_rect(topleft=i.pos)
-            screen.blit(i.surf, i.rect)
-
-def draw_food(rows, screen, dis):
-    foodx=random.randrange(rows)
-    foody=random.randrange(rows*h/w)
-    pygame.draw.rect(screen, GREEN, [foodx*40, foody*40, dis, dis])
-
-#def draw_gameover(screen, w, h):
-#    screen.fill(BLACK)
-#    font = pygame.font.SysFont('umeminchos3', 50)
-#    img = font.render('Потрачено', True, WHITE)
-#    img1 = font.render('Счет:' , True, WHITE)
-#    screen.blit(img, (w/2-100, h/2-50))
-#    screen.blit(img1, (w/2-100, h/2))
+            if i != self.body[len(self.body) - 1]: #кроме головы
+                i.surf = pygame.Surface((dis, dis))
+                i.surf.fill(pygame.Color('#D03E3A'))
+                i.rect = i.surf.get_rect(topleft=i.pos)
+                screen.blit(i.surf, i.pos)
+            else: #голова
+                i.surf = pygame.Surface((dis, dis))
+                i.surf.fill(pygame.Color('#FF0000'))
+                i.rect = i.surf.get_rect(topleft=i.pos)
+                screen.blit(i.surf, i.pos)
 
 
+class Food:
+    def __init__(self, w, h):
+        self.x = w // 2
+        self.y = w // 2
+
+    def rand(self): #рандомайзер еды
+        self.x = random.randrange(1, rows) * dis
+        self.y = random.randrange(1, rows) * dis
+
+    def draw(self, screen, dis): #отрисовка еды
+        pygame.draw.rect(screen, pygame.Color('#3AD044'), [self.x, self.y, dis, dis])
+
+    def eat(self, snakex, snakey): #если съел
+        if snakex == self.x and snakey == self.y:
+            self.rand()
+            screen.fill(pygame.Color('#A5FFAB'))
+            s.add_Cube()
+            print('eated')
 
 
-
-def draw_grid(w, rows, surface):
+def draw_grid(w, rows, surface): #сетка
     size_btwn = w // rows
 
     x = 0
@@ -99,8 +96,14 @@ def draw_grid(w, rows, surface):
         pygame.draw.line(surface, GREY, (0, y), (w, y))
 
 
-def re_dir(events):
-    for i in events:
+s = Snake() #инициализация анаконды
+f = Food(w, w) #яблОчко
+while sn_running:
+    pygame.display.update()
+    clock.tick(fps)
+    screen.fill(pygame.Color('#EFFFA5'))
+
+    for i in pygame.event.get(): # изменение направления и приращение попы на пробел
         if i.type == pygame.KEYDOWN:
             if i.key == pygame.K_UP and dir != [0, 1]:
                 dir = [0, -1]
@@ -110,19 +113,17 @@ def re_dir(events):
                 dir = [-1, 0]
             if i.key == pygame.K_RIGHT and dir != [-1, 0]:
                 dir = [1, 0]
-
-s = Snake()
-
-while sn_running:
-    pygame.display.update()
-    clock.tick(fps)
-    screen.fill(BLACK)
-    draw_grid(w, rows, screen)
-    s.move()
-    s.draw()
-#    draw_gameover(screen, w, h)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+            if i.key == pygame.K_SPACE:
+                s.add_Cube()
+        if i.type == pygame.QUIT:
             exit()
+    f.draw(screen, dis) #отрисовка еды
+    f.eat(s.body[len(s.body) - 1].pos[0], s.body[len(s.body) - 1].pos[1]) #проверка на съедение
+    s.move() #хуюв
+    for i in s.body[:-1]: #проверка на самопересечение
+        if s.head.pos == i.pos:
+            screen.fill(pygame.Color('#FF8A87'))
+            print('fuck')
 
-#
+    s.draw() #хуёу
+    draw_grid(w, rows, screen)
